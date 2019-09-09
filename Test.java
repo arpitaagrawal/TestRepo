4e8e66866e44741956db5b32009a3c7866c6b8cc
arpitaagrawal <arpitaag@usc.edu>
2019-09-08 20:11:20 -0700

    Attempte 2

--- Test.java
+++ Test.java
@@ -4,6 +4,7 @@ public class Test{
 
 		System.out.println("I will learn how to branch this time!!");
                 System.out.println("Adding more commits in the master branch and then will test the sub branch too!");
+		System.out.println("Attempt number 2");
 	}
 
 
