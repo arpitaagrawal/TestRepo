c2fa79c7afb4658999275f0cb9c2f33598b801b5
arpitaagrawal <arpitaag@usc.edu>
2019-09-08 20:48:32 -0700

    Master commit 1

--- Test.java
+++ Test.java
@@ -4,7 +4,7 @@ public class Test{
 
 		System.out.println("I will learn how to branch this time!!");
                 System.out.println("Adding more commits in the master branch and then will test the sub branch too!");
-		System.out.println("Attempt number 3.commit 3.");
+ 		System.out.println("Attempt number 3.commit 3.");
 	}
 
 
