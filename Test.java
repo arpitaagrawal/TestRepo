d1df26e9d43ebe87e52b61b696b45fd804c5abb6
arpitaagrawal <arpitaag@usc.edu>
2019-09-08 20:50:23 -0700

    Master commit 2

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test{
 
-	public static void main(String[] args){
+ 	public static void main(String[] args){
 
 		System.out.println("I will learn how to branch this time!!");
                 System.out.println("Adding more commits in the master branch and then will test the sub branch too!");
