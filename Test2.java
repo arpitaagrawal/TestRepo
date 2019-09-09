ebfa0b2706613068a261d41c3016915868432f82
arpitaagrawal <arpitaag@usc.edu>
2019-09-08 20:49:50 -0700

    Child branch comit 1
    Child branch comit 2
    
    Child branch comit 2

--- Test2.java
+++ Test2.java
@@ -1,7 +1,8 @@
 public class Test2{
 
-	public static void main(String[] args){
+ 	 public static void main(String[] args){
 		System.out.println("Commit 2. But should be placed at the third location");
+ 
 	}
 
 }
